c155918570edc7632ba58d2aa95cf2a6bd838038
ljan31 <mercen2021@gmail.com>
2026-01-13 21:05:12 -0400

    entities usuario, roles

---
+++ src/main/java/com/proyecto/fhce/library/entities/Usuario.java
@@ -0,0 +1,135 @@
+package com.proyecto.fhce.library.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "users")
+public class Usuario {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(unique = true, nullable = false)
+  private Long id_usuario;
+
+  @Column(unique = true)
+  private String username;
+
+  @Column(length = 60)
+  private String password;
+
+  @ManyToMany
+  @JsonIgnoreProperties({ "users", "handler", "hibernateLazyInitializer" })
+  @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"), uniqueConstraints = {
+      @UniqueConstraint(columnNames = { "user_id", "role_id" }) })
+  private List<Role> roles;
+
+  private boolean enabled;
+
+  @Transient
+  // @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  private boolean admin;
+
+  @PrePersist
+  public void prePersist() {
+    enabled = true;
+  }
+
+  public Usuario() {
+    roles = new ArrayList<>();
+  }
+
+  public Long getId_usuario() {
+    return id_usuario;
+  }
+
+  public void setId_usuario(Long id_usuario) {
+    this.id_usuario = id_usuario;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public List<Role> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(List<Role> roles) {
+    this.roles = roles;
+  }
+
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
+
+  public boolean isAdmin() {
+    return admin;
+  }
+
+  public void setAdmin(boolean admin) {
+    this.admin = admin;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id_usuario == null) ? 0 : id_usuario.hashCode());
+    result = prime * result + ((username == null) ? 0 : username.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Usuario other = (Usuario) obj;
+    if (id_usuario == null) {
+      if (other.id_usuario != null)
+        return false;
+    } else if (!id_usuario.equals(other.id_usuario))
+      return false;
+    if (username == null) {
+      if (other.username != null)
+        return false;
+    } else if (!username.equals(other.username))
+      return false;
+    return true;
+  }
+
+}
