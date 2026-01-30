ef2d84b7e06cebf7357d4c4180bcf89b7eae20cf
ljan31 <mercen2021@gmail.com>
2026-01-29 23:19:42 -0400

    update login

--- src/main/java/com/proyecto/fhce/library/dto/request/LoginRequest.java
+++ src/main/java/com/proyecto/fhce/library/dto/request/LoginRequest.java
@@ -8,4 +8,21 @@ public class LoginRequest {
 
   @NotBlank(message = "Password es requerido")
   private String password;
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
 }
--- src/main/java/com/proyecto/fhce/library/dto/response/LoginResponse.java
+++ src/main/java/com/proyecto/fhce/library/dto/response/LoginResponse.java
@@ -10,4 +10,61 @@ public class LoginResponse {
   private String email;
   private Set<String> roles;
   private PersonaResponse persona;
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Set<String> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(Set<String> roles) {
+    this.roles = roles;
+  }
+
+  public PersonaResponse getPersona() {
+    return persona;
+  }
+
+  public void setPersona(PersonaResponse persona) {
+    this.persona = persona;
+  }
+
 }
